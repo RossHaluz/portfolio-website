526bf3e938883091fc197c4272d3815505265974
Ecommerce <rossgaluzinskiy@gmail.com>
2025-11-05 22:28:22 +0200

    add

--- components/TransitionProvider.jsx
+++ components/TransitionProvider.jsx
@@ -2,47 +2,52 @@
 import { AnimatePresence, motion } from "framer-motion";
 import Navbar from "./Navbar";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
 
 const TransitionProvider = ({ children }) => {
   const pathname = usePathname();
-  const [showTitle, setShowTitle] = useState(true);
 
   return (
-    <AnimatePresence mode="wait">
-      <div
-        key={pathname}
-        className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100"
-      >
-        <motion.div
-          className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40"
-          animate={{ height: "0vh" }}
-          exit={{ height: "140vh" }}
-          transition={{ duration: 0.5, ease: "easeOut" }}
-        />
+    // <AnimatePresence mode="wait">
+    //   <div
+    //     key={pathname}
+    //     className="w-screen h-sreen bg-gradient-to-b from-blue-100 to-red-100"
+    //   >
+    //     <motion.div
+    //       className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40"
+    //       animate={{ height: "0vh" }}
+    //       exit={{ height: "140vh" }}
+    //       transition={{ duration: 0.6, ease: "easeOut" }}
+    //     />
 
-        <motion.div
-          className="fixed top-0 bottom-0 right-0 left-0 m-auto text-8xl text-white cursor-default z-50 w-fit h-fit"
-          initial={{ opacity: 1 }}
-          animate={{ opacity: 0 }}
-          exit={{ opacity: 0, display: "none" }}
-          transition={{ duration: 0.8, ease: "easeOut" }}
-        >
-          {pathname?.substring(1)}
-        </motion.div>
+    //     <motion.div
+    //       className="fixed top-0 bottom-0 right-0 left-0 m-auto text-8xl text-white cursor-default z-50 w-fit h-fit"
+    //       initial={{ opacity: 1 }}
+    //       animate={{ opacity: 0 }}
+    //       exit={{ opacity: 0, display: "none" }}
+    //       transition={{ duration: 0.7, ease: "easeOut" }}
+    //     >
+    //       {pathname?.substring(1)}
+    //     </motion.div>
 
-        <motion.div
-          className="h-screen w-screen fixed bg-black rounded-t-[100px] bottom-0 z-40"
-          initial={{ height: "140vh" }}
-          animate={{ height: "0vh", transition: { delay: 0.6 } }}
-        />
-        <div className="h-24">
-          <Navbar />
-        </div>
-        <main className="h-[calc(100vh-6rem)]">{children}</main>
+    //     <motion.div
+    //       className="h-screen w-screen fixed bg-black rounded-t-[100px] bottom-0 z-40"
+    //       initial={{ height: "140vh" }}
+    //       animate={{ height: "0vh" }}
+    //       transition={{ duration: 0.6, ease: "easeInOut", delay: 0.1 }}
+    //     />
+    //     <div className="h-24">
+    //       <Navbar />
+    //     </div>
+    //     <main className="h-[calc(100vh-6rem)]">{children}</main>
+    //   </div>
+    // </AnimatePresence>
+    <>
+      {" "}
+      <div className="h-24">
+        <Navbar />
       </div>
-    </AnimatePresence>
-
+      <main className="h-[calc(100vh-6rem)]">{children}</main>
+    </>
   );
 };
 
